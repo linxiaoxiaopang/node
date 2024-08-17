9994a83665877c61f64f8890f44697093d6a7483
SmartLin <linguangyan@cnzhengding.com>
2024-08-17 17:06:51 +0800

    优化

--- selenium/index.js
+++ selenium/index.js
@@ -1,33 +1,42 @@
-const {Builder, By, Key, until} = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-const path = require('path');
-const {whileWait, waitTimeByNum} = require('./utils');
-// superb / 2022@zhengdingZD
-// 创建 WebDriver 实例
-(async function example() {
-    // 设置 Chrome 浏览器选项
-    let options = new chrome.Options();
-    options.addArguments('start-maximized'); // 启动时最大化窗口
+const { Builder, By, Key, until } = require('selenium-webdriver')
+const chrome = require('selenium-webdriver/chrome')
+const path = require('path')
+const RE_DESIGN_URL = 'https://www.zhengdingyunshang.com/#/design/designContainer?protoId=72&productId=4562501'
+const USER_NAME = 'superb'
+const USER_PASSWORD = '2022@zhengdingZD'
+const picList = [['76468770664341'], ['CT24081604'], ['081601m'], ['JYPAI1711']]
+const { whileWait, waitTimeByNum, getSystemUrls } = require('./utils')
+  // superb / 2022@zhengdingZD
+  // 创建 WebDriver 实例
+;(async function example() {
+  // 设置 Chrome 浏览器选项
+  let options = new chrome.Options()
+  options.addArguments('start-maximized') // 启动时最大化窗口
+  console.log(1)
+  // 初始化 WebDriver
+  let driver = await new Builder().forBrowser('chrome').setChromeOptions(options).build()
+  console.log(2)
+  try {
+    const urlList = getSystemUrls(RE_DESIGN_URL)
+    // 打开网页
+    driver.get(urlList.login)
+    //登录
+    // const userName = await querySelector('[uiid="zd-name"]')
+    // userName.sendKeys(USER_NAME)
+    // const password = await querySelector('[uiid="zd-pwd"]')
+    // password.sendKeys(USER_PASSWORD)
+    //跳转到topic页面
+    const currentUrl0 = urlList.login
+    await whileWait(async () => {
+      let currentUrl1 = await driver.getCurrentUrl()
+      return currentUrl0 != currentUrl1
+    })
 
-    // 初始化 WebDriver
-    let driver = await new Builder()
-        .forBrowser('chrome')
-        .setChromeOptions(options)
-        .build()
-
-    try {
-        // 打开网页
-        await driver.get('https://www.zhengdingyunshang.com/#/login')
-        let currentUrl = await driver.getCurrentUrl()
-        await whileWait(async () => {
-            let currentUrl1 = await driver.getCurrentUrl()
-            return currentUrl1 != currentUrl
-        })
-        console.log(1)
-        await driver.get('https://www.zhengdingyunshang.com/#/design/designContainer?protoId=72&productId=4562501')
-        await pageLoaded()
-        await waitTimeByNum(3000)
-        let designData = await driver.executeScript(`
+    console.log(1)
+    await driver.get(urlList.reDesign)
+    await pageLoaded()
+    await waitTimeByNum(3000)
+    const designData = await driver.executeScript(`
            let element = document.querySelector('.designContainerPage')
            console.log('element', element);
            const fabricList = element.__vue__.fabricList
@@ -47,63 +56,83 @@ const {whileWait, waitTimeByNum} = require('./utils');
             })
            })
         `)
-        // console.log('designData', designData)
-        await driver.get('https://www.zhengdingyunshang.com/#/design/index')
-        await waitTimeByNum(200)
-        await driver.get('https://www.zhengdingyunshang.com/#/design/designContainer?protoId=72')
+    // console.log('designData', designData)
+    await driver.get(urlList.topic)
+    await waitTimeByNum(200)
+    await driver.get(urlList.design)
 
-        await pageLoaded()
-        // 查找 searchIcon 元素并点击
-        let searchIcon = await driver.findElement(By.css('.cate-and-search-component .el-icon-search')) // 根据实际情况更改选择器
+    await pageLoaded()
+
+    for (let i = 0; i < picList.length; i++) {
+
+      let designsEls = await querySelectorAll('.designContainerHeader .design')
+      if (designsEls.length > 1) {
+        const chunkDesignEl = designsEls[1]
+        let classAttribute = await chunkDesignEl.getAttribute('class')
+        // 检查是否包含特定的 active 激活样式，检测是否是专业设计高亮
+        let isChunkDesignActive = classAttribute.indexOf('active') >= 0
+        if (isChunkDesignActive) {
+          await designsEls[0].click()
+          await whileWait(async () => {
+            let submitBtn = await querySelector('.el-message-box__btns .el-button--primary')
+            if (!submitBtn) return false
+            await submitBtn.click()
+            return true
+          })
+        }
+      }
+
+      const picTitleList = picList[i]
+      // 查找 searchIcon 元素并点击
+      let pictureTitleElement = await querySelector('.cate-and-search-component .search input')
+      if (!pictureTitleElement) {
+        let searchIcon = await querySelector('.cate-and-search-component .el-icon-search')
         await searchIcon.click()
-
         // 查找 input 元素并输入文本
-        let inputElement = await driver.findElement(By.css('.cate-and-search-component .search input'))
-        await inputElement.sendKeys('76468770664341')
+        pictureTitleElement = await querySelector('.cate-and-search-component .search input')
+      }
+      let firstPicTitle = ''
+      for (let j = 0; j < picTitleList.length; j++) {
+        const picTitle = picTitleList[j]
+        if(!firstPicTitle) firstPicTitle = picTitle
+        await pictureTitleElement.clear()
+        await pictureTitleElement.sendKeys(picTitle)
         //点击图片
         await whileWait([
-                async () => {
-                    try {
-                        await driver.findElement(By.css('.uploadDesignPicComponent .loading-wrapper'))
-                        return true
-                    } catch {
-                        return false
-                    }
-                },
-                async () => {
-                    try {
-                        await driver.findElement(By.css('.uploadDesignPicComponent .loading-wrapper'))
-                        return false
-                    } catch {
-                        return true
-                    }
-                }
-            ]
+            () => {
+              return querySelector('.uploadDesignPicComponent .loading-wrapper')
+            },
+            async () => {
+              return !(await querySelector('.uploadDesignPicComponent .loading-wrapper'))
+            }
+          ]
         )
         await waitTimeByNum(20)
         //点击图片
-        let img = await driver.findElement(By.css('.hover-pic-popup-component .autoImgComponent')) // 根据实际情况更改选择器
+        const img = await querySelector('.hover-pic-popup-component .autoImgComponent')
+        if (!img) continue
         await img.click()
-        await canvasRendered()
+        await waitTimeByNum(20)
+      }
 
-        // 查找所有具有相同 class 的元素
-        try {
-            let designs = await driver.findElements(By.css('.designContainerHeader .design')); // 根据实际情况更改 class 名称
-            designs[1].click()
-            // 点击模式切换按钮
-            await whileWait(async () => {
-                try {
-                    let submitBtn = await driver.findElement(By.css('.el-message-box__btns .el-button--primary')) // 根据实际情况更改选择器
-                    await submitBtn.click()
-                    return true
-                } catch {
-                    return false
-                }
-            })
-            await canvasRendered()
-        } catch {
-        }
-        await driver.executeScript(`
+      //画布加载渲染
+      await canvasRendered()
+
+      // 获取一键定制，专业定制按钮
+      if (designsEls.length > 1) {
+        designsEls[1].click()
+        // 点击模式切换按钮
+        await whileWait(async () => {
+          let submitBtn = await querySelector('.el-message-box__btns .el-button--primary')
+          if (!submitBtn) return false
+          await submitBtn.click()
+          return true
+        })
+        await canvasRendered()
+      }
+
+      //根据模板数据更新画布数据
+      await driver.executeScript(`
            let element = document.querySelector('.designContainerPage')
            console.log('element', element);
            const fabricList = element.__vue__.fabricList
@@ -114,61 +143,108 @@ const {whileWait, waitTimeByNum} = require('./utils');
              os.map((sItem, sIndex) => {
                 const formDataList = ${JSON.stringify(designData)}
                 const formData = formDataList[index][sIndex]
-                console.log('formData', formData)
-                sItem.setOptions({
-                   left: formData.left,
-                   top: formData.top,
-                   scaleX: formData.scaleX,
-                   scaleY: formData.scaleY,
-                   flipX: formData.flipX,
-                   flipY: formData.flipY,
-                   angle: formData.angle
-                })
+                if(!formData) {
+                  canvas.remove(sItem)
+                } else {
+                   sItem.setOptions({
+                       left: formData.left,
+                       top: formData.top,
+                       scaleX: formData.scaleX,
+                       scaleY: formData.scaleY,
+                       flipX: formData.flipX,
+                       flipY: formData.flipY,
+                       angle: formData.angle
+                   })
+                }
                 canvas.renderAll()
                 canvas.$cacheFrontDesignData = null
              })
            })
         `)
 
+      //下一步
+      const nextStepBtn = await querySelector('.done-btn-wrapper .el-button--primary')
+      nextStepBtn.click()
 
-        // 查找所有具有相同 class 的元素
-        let nextStepBtn = await driver.findElement(By.css('.done-btn-wrapper .el-button--primary'))
-        nextStepBtn.click()
+      await waitTimeByNum(200)
 
+      //露白弹窗 继续定制
+      const keepDesignBtn = await querySelector('.el-message-box__btns .el-button--default')
+      if (keepDesignBtn) {
+        await keepDesignBtn.click()
         await waitTimeByNum(200)
+      }
 
-        try {
-            let keepDesignBtn = await driver.findElement(By.css('.el-message-box__btns .el-button--default')) // 根据实际情况更改选择器
-            await keepDesignBtn.click()
-            await waitTimeByNum(200)
-        } catch {
+      //输入前缀
+      await whileWait(async () => {
+        const skuPrefixEl = await querySelector('[uiid="zd-skuPrefix"]')
+        if (skuPrefixEl) {
+          await skuPrefixEl.sendKeys(firstPicTitle)
+          return true
         }
+        return false
+      })
 
-        // 查找所有具有相同 class 的元素
-        let saveBtn = await driver.findElement(By.css('.save-component-dialog_custom-class .el-button--primary'))
-        saveBtn.click()
+      //保存定制
+      const saveBtn = await querySelector('.save-component-dialog_custom-class .el-button--primary')
+      saveBtn.click()
 
-    } finally {
-        // 关闭浏览器
-        // await driver.quit();
+      let keepToDesignEl = null
+      await whileWait(async () => {
+        const isError = await querySelector('.el-form-item__error')
+        if (isError) return true
+        keepToDesignEl = await querySelector('.uiid-zd-success-cancel')
+        if (keepToDesignEl) return true
+        return false
+      })
+      //保存成功
+      if (keepToDesignEl) {
+        keepToDesignEl.click()
+      } else {
+        await driver.get(urlList.topic)
+        await waitTimeByNum(200)
+        await driver.get(urlList.design)
+        await pageLoaded()
+      }
     }
+  } finally {
+    // 关闭浏览器
+    // await driver.quit();
+  }
 
-    async function pageLoaded() {
-        await whileWait(async () => {
-            return await driver.executeScript(`
+  async function querySelector(selector) {
+    try {
+      return await driver.findElement(By.css(selector))
+    } catch {
+      console.log('获取不到元素')
+      return null
+    }
+  }
+
+  async function querySelectorAll(selector) {
+    try {
+      return await driver.findElements(By.css(selector))
+    } catch {
+      return []
+    }
+  }
+
+  async function pageLoaded() {
+    await whileWait(async () => {
+      return await driver.executeScript(`
                 let element = document.querySelector('.designContainerPage')
                 if (!element) return false
                 const context = element.__vue__
                 console.log('context.loading', context.loading)
                 return !context.loading
             `
-            )
-        })
-    }
+      )
+    })
+  }
 
-    async function canvasRendered() {
-        await whileWait(async () => {
-            return await driver.executeScript(`
+  async function canvasRendered() {
+    await whileWait(async () => {
+      return await driver.executeScript(`
                 console.log('canvasRendered')
                 let element = document.querySelector('.designContainerPage')
                 if (!element) return false
@@ -176,8 +252,8 @@ const {whileWait, waitTimeByNum} = require('./utils');
                 console.log('context.loading', context.loading)
                 return !context.loading
             `
-            )
-        })
-    }
+      )
+    })
+  }
 })()
 
--- selenium/utils.js
+++ selenium/utils.js
@@ -1,9 +1,9 @@
 async function whileWait(callbackList, interval = 300) {
   if (!callbackList) return true
-  if(!Array.isArray(callbackList)) {
+  if (!Array.isArray(callbackList)) {
     callbackList = [callbackList]
   }
-  for(let i = 0; i < callbackList.length; i++) {
+  for (let i = 0; i < callbackList.length; i++) {
     let isFinished = false
     const callback = callbackList[i]
     while (!isFinished) {
@@ -21,7 +21,51 @@ async function waitTimeByNum(num) {
   })
 }
 
+function parseUrl(urlString) {
+  const url = new URL(urlString)
+  const { origin, hash, searchParams, href, pathname } = url
+  const res = {
+    href,
+    origin
+  }
+  const isHash = pathname == '/' && hash
+  if (!isHash) {
+    res.rootUrl = `${origin}/`
+    res.pathname = pathname
+    res.searchParams = searchParams
+    res.mode = 'history'
+  } else {
+    const cleanHash = hash.substring(1)
+    let [hashPathname, queryString] = cleanHash.split('?')
+    const queryParams = new URLSearchParams(queryString)
+    res.rootUrl = `${origin}/#/`
+    res.pathname = `/#${hashPathname}`
+    res.searchParams = queryParams
+    res.mode = 'hash'
+  }
+  res.searchParamsObj = {}
+  for (const [key, value] of res.searchParams) {
+    res.searchParamsObj[key] = value
+  }
+
+  res.originAndPathname = `${res.origin}${res.pathname}`
+  return res
+}
+
+
+function getSystemUrls(urlString) {
+  const { rootUrl, searchParamsObj, originAndPathname } = parseUrl(urlString)
+  return {
+    login: `${rootUrl}login`,
+    topic: rootUrl,
+    design: `${originAndPathname}?protoId=${searchParamsObj.protoId}`,
+    reDesign: `${originAndPathname}?protoId=${searchParamsObj.protoId}&productId=${searchParamsObj.productId}`
+  }
+}
+
 module.exports = {
   whileWait,
-  waitTimeByNum
+  waitTimeByNum,
+  parseUrl,
+  getSystemUrls
 }
